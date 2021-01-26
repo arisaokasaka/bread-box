6ad1a194ea96c9194928920b2cc5b4d3ed51e827
arisa <arisa.okasaka@gmail.com>
2021-01-26 22:52:58 +0900

    営業時間のリストを作成

---
+++ bread-app/resources/ts/info/Hours.ts
@@ -0,0 +1,11 @@
+class Hours {
+    
+    hours: any;
+    
+    constructor() {
+        this.hours = [ "現在営業中", "朝9時までにオープン", "19時以降も営業"];
+    }
+}
+
+const hours = new Hours;
+export default hours;
