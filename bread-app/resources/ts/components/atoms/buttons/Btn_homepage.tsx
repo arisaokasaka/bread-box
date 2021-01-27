bf12b4cfd74194003925c3f35332b5355393996f
arisa <arisa.okasaka@gmail.com>
2021-01-27 16:06:20 +0900

    ホームページ用のボタン作成

---
+++ bread-app/resources/ts/components/atoms/buttons/Btn_homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faPaperPlane} from "@fortawesome/free-solid-svg-icons";
+
+type StoreInfoProps = ({
+    StoreInfo: any
+})
+
+const Btn_homepage: React.FC<StoreInfoProps> = ({StoreInfo}) =>(
+    <div className = "a-btn-homepage">
+        <a href={StoreInfo.url}><FontAwesomeIcon icon={faPaperPlane}/>&nbsp;ホームページを見る</a>
+    </div>
+)
+
+export default Btn_homepage;
