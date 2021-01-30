3304ae8b90f991254aa5619511fb9c55c8671b1b
arisa <arisa.okasaka@gmail.com>
2021-01-30 22:38:07 +0900

    お店の基本情報コンポーネント実装

--- bread-app/resources/ts/components/molecules/Store_basicInfo.tsx
+++ bread-app/resources/ts/components/molecules/Store_basicInfo.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import Btn_favorite from '../atoms/buttons/Btn_favorite';
 import Btn_interested from '../atoms/buttons/Btn_interested';
+import Store_subInfo from './Store_subinfo';
 
 type StoreInfoProps = ({
     StoreInfo? : any;
 })
 
 const Store_basicInfo: React.FC<StoreInfoProps> = ({StoreInfo}) => (
-    <div className = "m-store__basicInfo">
-        <div className = "m-store__basicInfo__btn">
+    <div className = "m-store-basicInfo">
+        <div className = "m-store-basicInfo__btn">
             <Btn_favorite />
             <Btn_interested />
         </div>
         {StoreInfo.map((el)=>{
             return(
             <div>
-                <h2 className = "m-store__basicInfo__name">{el.name}</h2>
-                <p className = "m-store__basicInfo__address">{el.address}</p>
-                <p className = "m-store__basicInfo__message">{el.message}</p>
+                <h2 className = "m-store-basicInfo__name">{el.name}</h2>
+                <p className = "m-store-basicInfo__address">{el.address}</p>
+                <p className = "m-store-basicInfo__message">{el.message}</p>
             </div>
         );})}
+        {/* モバイル用のsubInfo */}
+        <Store_subInfo
+            StoreInfo = {StoreInfo}
+        />
     </div>
 );
 
