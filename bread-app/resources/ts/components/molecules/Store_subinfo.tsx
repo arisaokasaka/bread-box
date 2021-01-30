06e0a941f4cf30c0020f8fe5b2fde834521afdee
arisa <arisa.okasaka@gmail.com>
2021-01-30 22:38:28 +0900

    お店のサブ情報コンポーネント実装

--- bread-app/resources/ts/components/molecules/Store_subinfo.tsx
+++ bread-app/resources/ts/components/molecules/Store_subinfo.tsx
@@ -2,31 +2,51 @@ import React from 'react'
 import Score from '../atoms/Score';
 import Week from '../../info/Week';
 import Schedule from '../atoms/Schedule';
+import Btn_homepage from '../atoms/buttons/Btn_homepage';
+
+import ModalSNS from '../atoms/modal/Modal_sns';
 
 type StoreInfoProps = ({
     StoreInfo? : any;
 })
 
+let testStoreInfo = [
+    {
+        star: 2.5,
+        url: "https://de-milestones.com/",
+        sns: {
+            twitter: "https://twitter.com/dev_design8",
+            instagram: "ss",
+            facebook: "ss",
+            other: "Ss"
+        },
+    }    
+]
+
 const Store_subInfo: React.FC<StoreInfoProps> = ({StoreInfo}) => (
-    <div className = "m-store__subInfo">
+    <div className = "m-store-subInfo">
         {StoreInfo.map((el)=>{
+            el.url = 1;
             return(
-            <div className = "m-store__subInfo__container">
-                <Score/>
-                <div className = "m-store__subInfo__container__businessday">
-                    <label>営業日・営業時間</label>
+            <div className = "m-store-subInfo__container">
+                <div className = "m-store-subInfo__container__item">
+                    <Score
+                        ScoreStar = {testStoreInfo}
+                    />
+                </div>
+                <div className = "m-store-subInfo__container__item">
                     <Schedule
                         Week = {Week.week}
                     />
                     <p>{el.business_memo}</p>
                 </div>
-                <div className = "m-store__subInfo__container__homepage">
-                    <label>ホームページ</label>
-                    <p>{el.url}</p>
-                </div>
-                <div className = "m-store__subInfo__container__sns">
-                    <label>SNS</label>
-                    <p>{el.sns}</p>
+                <div className = "m-store-subInfo__container__item__btns">
+                    <Btn_homepage
+                        StoreInfo = {testStoreInfo}
+                    />
+                    <ModalSNS
+                        StoreInfo = {testStoreInfo}
+                    />
                 </div>
             </div>
             );
