ddf4e1d8d18d0b5d5f0c373ff509acb5a97bfa6d
arisa <arisa.okasaka@gmail.com>
2021-02-08 12:14:36 +0900

    ボタンスタイル調整

--- bread-app/resources/ts/components/atoms/buttons/Btn_favorite.tsx
+++ bread-app/resources/ts/components/atoms/buttons/Btn_favorite.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
 
 export default function Btn_favorite() {
     return (
         <button className = "a-btn-favorite">
-            <span>♥</span><span>お気に入り</span>
+            <span><FontAwesomeIcon icon={faHeart}/></span>
+            <span>お気に入り</span>
         </button>
     )
 }
--- bread-app/resources/ts/components/atoms/buttons/Btn_interested.tsx
+++ bread-app/resources/ts/components/atoms/buttons/Btn_interested.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFlag} from '@fortawesome/free-solid-svg-icons';
 
 export default function Btn_interested() {
     return (
         <button className="a-btn-interested">
-            <span>★</span><span>行ってみたい</span>
+            <span><FontAwesomeIcon icon={faFlag}/></span>
+            <span>行ってみたい</span>
         </button> 
     )
 }
