147e4532dd7df5a2324b4a1ffdd3d8f8f8e0b6ac
arisa <arisa.okasaka@gmail.com>
2021-02-09 16:49:10 +0900

    お気に入りボタンと行ってみたいボタン：未ログイン時はログイン画面に飛ばし、店舗ログイン時は非表示

--- bread-app/resources/ts/components/atoms/buttons/Btn_favorite.tsx
+++ bread-app/resources/ts/components/atoms/buttons/Btn_favorite.tsx
@@ -4,17 +4,18 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faHeart} from '@fortawesome/free-solid-svg-icons';
 import { UserAuthContext } from '../../../contexts/UserAuthContext';
 
-
 export default function Btn_favorite() {
     const { state } = useContext(UserAuthContext);
     let BtnFavorite: any;
-    if(state.uuid){
+    if(state.uuid && state.auth==="user"){
         BtnFavorite = (
         <button className = "a-btn-favorite">
             <span><FontAwesomeIcon icon={faHeart}/></span>
             <span>お気に入り</span>
         </button>
         );
+    }else if(state.uuid && state.auth==="store"){
+        BtnFavorite = null;
     }else{
         BtnFavorite = (
             <Link to="/login_user" className = "a-btn-favorite">
