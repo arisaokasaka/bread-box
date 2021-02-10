dedd8718e741228d191c1bdd7478dbf56d75a57c
arisa <arisa.okasaka@gmail.com>
2021-02-10 17:28:02 +0900

    ホームページ部分のスタイル調整

--- bread-app/resources/ts/components/molecules/storeEdit/storeEditBasic/EditHomepage.tsx
+++ bread-app/resources/ts/components/molecules/storeEdit/storeEditBasic/EditHomepage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useForm} from 'react-hook-form';
+import BtnSave from '../../../atoms/buttons/BtnSave';
 
 type BasicProps = ({
     StoreInfo: Array<any>;
@@ -17,11 +18,20 @@ const EditHomepage: React.FC<BasicProps> = ({StoreInfo}) => {
             {StoreInfo.map((el)=>{
                 return(
                     <div className = "m-storeEdit-homepage__container" key = {el.uuid}>
-                        <form className="m-storeEdit-homepage__container__form" onSubmit={handleSubmit(onSubmit)}>
-                            <label htmlFor="store_url">ホームページURL</label>
-                            <input type="url" id="store_url" name="url" value={el.url} ref={ register} />
-                            <div className="m-storeEdit-homepage__container__form__btn">
-                                <input type="submit" value="変更する"/>
+                        <h3>ホームページ</h3>
+                        <a>ホームページのURLを入力してください</a>
+                        <form className="m-storeEdit-homepage__container__form m-storeForm" onSubmit={handleSubmit(onSubmit)}>
+                            <div className="m-storeForm__item">
+                                <label htmlFor="store_url">ホームページURL</label>
+                                <div className="m-storeForm__item__input">
+                                    <input type="url" id="store_url" name="url" value={el.url} ref={register({required: true})} />
+                                </div>
+                            </div>
+                            <div className="m-storeEdit-homepage__container__form__btn m-storeForm__btn">
+                                <BtnSave
+                                    InputType={"submit"}
+                                    OnClickFunction={null}
+                                />
                             </div>
                         </form>
                     </div>
