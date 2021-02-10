67afa19a9ed7da26ff27f87fb256fb1cd25c6477
arisa <arisa.okasaka@gmail.com>
2021-02-10 17:28:17 +0900

    営業のおしらせ部分のスタイル調整

--- bread-app/resources/ts/components/molecules/storeEdit/storeEditBasic/EditBusinessMemo.tsx
+++ bread-app/resources/ts/components/molecules/storeEdit/storeEditBasic/EditBusinessMemo.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useForm} from 'react-hook-form';
+import BtnSave from '../../../atoms/buttons/BtnSave';
 
 type BasicProps = ({
     StoreInfo: Array<any>;
@@ -17,12 +18,20 @@ const EditBusinessMemo: React.FC<BasicProps> = ({StoreInfo}) => {
             {StoreInfo.map((el)=>{
                 return(
                     <div className = "m-storeEdit-businessMemo__container" key = {el.uuid}>
-                        <form className="m-storeEdit-businessMemo__container__form" onSubmit={handleSubmit(onSubmit)}>
-                            <label>営業日・営業時間備考</label>
-                            <span>【記載例】<br></br>定休日：第3水曜日<br></br>営業時間：月～水 9時～19時 / 木～土 8時～13時</span>
-                            <textarea name="business_memo" ref={register}/>
-                            <div className="m-storeEdit-businessMemo__container__form__btn">
-                                <input type="submit" value="変更する"/>
+                        <h3>営業に関するお知らせ</h3>
+                        <form className="m-storeEdit-businessMemo__container__form m-storeForm" onSubmit={handleSubmit(onSubmit)}>
+                            <div className="m-storeForm__item">
+                                <label>お知らせ内容</label>
+                                <div className="m-storeForm__item__input">
+                                    <span>【記載例】<br></br>定休日：第3水曜日<br></br>営業時間：月～水 9時～19時 / 木～土 8時～13時</span>
+                                    <textarea name="business_memo" ref={register({required: true})}/>
+                                </div>
+                            </div>
+                            <div className="m-storeEdit-businessMemo__container__form__btn m-storeForm__btn">
+                                <BtnSave
+                                    InputType={"submit"}
+                                    OnClickFunction={null}
+                                />
                             </div>
                         </form>
                     </div>
