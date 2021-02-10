f6fbf54e716a3a69d6cd9db8c95abbb85aca092f
arisa <arisa.okasaka@gmail.com>
2021-02-10 15:58:18 +0900

    店舗関連のフォームスタイルを一括管理（storeFormファイルで指定）

--- bread-app/resources/ts/components/molecules/storeEdit/storeEditBasic/EditBusinessDays.tsx
+++ bread-app/resources/ts/components/molecules/storeEdit/storeEditBasic/EditBusinessDays.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useForm} from 'react-hook-form';
 import InputSchedule from '../../InputSchedule';
+import BtnSave from '../../../atoms/buttons/BtnSave';
 
 type BasicProps = ({
     StoreInfo: Array<any>;
@@ -18,16 +19,28 @@ const EditBusinessDays: React.FC<BasicProps> = ({StoreInfo}) => {
             {StoreInfo.map((el)=>{
                 return(
                     <div className = "m-storeEdit-businessDay__container" key = {el.uuid}>
-                        <form className="m-storeEdit-businessDay__container__form" onSubmit={handleSubmit(onSubmit)}>
-                            <label htmlFor="business_day" className="a-label-required">営業日</label>
-                            <span>営業している曜日を全てチェックしてください。</span>
-                            <InputSchedule/>
-                            <label>営業日・営業時間備考</label>
-                            <span>【記載例】<br></br>定休日：第3水曜日<br></br>営業時間：月～水 9時～19時 / 木～土 8時～13時</span>
-                            <textarea name="business_memo" ref={register}/>
+                        <h3>営業日・営業時間</h3>
+                        <form className="m-storeEdit-businessDay__container__form m-storeForm" onSubmit={handleSubmit(onSubmit)}>
+                            <div className="m-storeForm__item">
+                                <label htmlFor="business_day" className="a-label-required__red">営業日</label>
+                                <div className="m-storeForm__item__input">
+                                    <span>営業している曜日を全てチェックしてください。</span>
+                                    <InputSchedule/>
+                                </div>
+                            </div>
+                            <div className="m-storeForm__item">
+                                <label>営業日・営業時間備考</label>
+                                <div className="m-storeForm__item__input">    
+                                    <span>【記載例】<br></br>定休日：第3水曜日<br></br>営業時間：月～水 9時～19時 / 木～土 8時～13時</span>
+                                    <textarea name="business_memo" ref={register}/>
+                                </div>
+                            </div>
                             
-                            <div className="m-storeEdit-businessDay__container__form__btn">
-                                <input type="submit" value="変更する"/>
+                            <div className="m-storeEdit-businessDay__container__form__btn m-storeForm__btn">
+                                <BtnSave
+                                    InputType={"submit"}
+                                    OnClickFunction={null}
+                                />
                             </div>
                         </form>
                     </div>
--- bread-app/resources/ts/components/page/register/Register_store.tsx
+++ bread-app/resources/ts/components/page/register/Register_store.tsx
@@ -45,35 +45,35 @@ export default function Register_store() {
                 <div className = "p-register-store__container__links">
                     <span>既に登録済ですか？<Link to="/login_store">店舗ログイン</Link></span>
                 </div>
-                <form className="p-register-store__container__form" name="form_storeRegister" onSubmit={handleSubmit(onSubmit)}>
-                    <div className = "p-register-store__container__form__item">
+                <form className="p-register-store__container__form m-storeForm" name="form_storeRegister" onSubmit={handleSubmit(onSubmit)}>
+                    <div className = "p-register-store__container__form__item m-storeForm__item">
                         <label htmlFor="store_name" className="a-label-required__red">店舗名</label>
-                        <div className = "p-register-store__container__form__item__input">
+                        <div className = "p-register-store__container__form__item__input m-storeForm__item__input">
                             <input type="text" id="store_name" name="name" ref={ register({required: true})} />
                             {errors.name && <p>店舗名は必須です。</p>}
                         </div>
                     </div>
 
-                    <div className = "p-register-store__container__form__item">
+                    <div className = "p-register-store__container__form__item m-storeForm__item">
                         <label htmlFor="store_email" className="a-label-required__red">メールアドレス</label>
-                        <div className = "p-register-store__container__form__item__input">
+                        <div className = "p-register-store__container__form__item__input m-storeForm__item__input">
                             <input type="email" name="email" id="store_email" ref={register({required: true})}/>
                             {errors.email && <p>メールアドレスは必須です。</p>}
                             {emailErrorMessage(emailError)}
                         </div>
                     </div>
 
-                    <div className = "p-register-store__container__form__item">
+                    <div className = "p-register-store__container__form__item m-storeForm__item">
                         <label htmlFor="store_address" className="a-label-required__red">住所</label>
-                        <div className = "p-register-store__container__form__item__input">
+                        <div className = "p-register-store__container__form__item__input m-storeForm__item__input">
                             <input type="text" name="address" id="store_address" ref={register({required: true})}/>
                             {errors.address && <p>住所は必須です。</p>}
                         </div>
                     </div>
 
-                    <div className = "p-register-store__container__form__item">
+                    <div className = "p-register-store__container__form__item m-storeForm__item">
                         <label htmlFor="store_tel" className="a-label-required__red">電話番号</label>
-                        <div className = "p-register-store__container__form__item__input">
+                        <div className = "p-register-store__container__form__item__input m-storeForm__item__input">
                             <span>半角・ハイフンなしで入力してください。</span>
                             <input type="text" name="tel" id="store_tel" ref={register({required: true, pattern: /[0-9]{10,11}/})}/>
                             {errors.tel && errors.tel.type === "required" && (<p>電話番号は必須です。</p>)}
@@ -81,27 +81,27 @@ export default function Register_store() {
                         </div>
                     </div>
 
-                    <div className = "p-register-store__container__form__item">
+                    <div className = "p-register-store__container__form__item m-storeForm__item">
                         <label htmlFor="store_password" className="a-label-required__red">パスワード</label>
-                        <div className = "p-register-store__container__form__item__input">
+                        <div className = "p-register-store__container__form__item__input m-storeForm__item__input">
                             <input type="password" name="password" id="store_password" ref={register({required: true, pattern: /[a-zA-Z0-9]{8,16}/})}/>
                             {errors.password && errors.password.type === "required" && (<p>パスワードは必須です。</p>)}
                             {errors.password && errors.password.type === "pattern" && (<p>8~16文字の半角英数字で指定してください。</p>)}
                         </div>
                     </div>
 
-                    <div className = "p-register-store__container__form__item">
+                    <div className = "p-register-store__container__form__item m-storeForm__item">
                         <label htmlFor="store_password-check" className="a-label-required__red">確認用パスワード</label>
-                        <div className = "p-register-store__container__form__item__input">
+                        <div className = "p-register-store__container__form__item__input m-storeForm__item__input">
                             <input type="password" name="password_check" id="store_password-check" ref={register({required: true})}/>
                             {errors.password_check && errors.password_check.type === "required" && (<p>確認用パスワードは必須です。</p>)}
                             {PasswordErrorMessage(getValues('password'),getValues('password_check'))}
                         </div>
                     </div>
 
-                    <div className = "p-register-store__container__form__item">
+                    <div className = "p-register-store__container__form__item m-storeForm__item">
                         <label htmlFor="business_day" className="a-label-required__red">営業日・営業時間</label>
-                        <div className = "p-register-store__container__form__item__input">
+                        <div className = "p-register-store__container__form__item__input m-storeForm__item__input">
                             <input type="hidden" name="business_day" ref={register} />
                             <span>営業している曜日をチェックのうえ、営業時間を入力してください。</span>
                             <InputSchedule/>
@@ -109,9 +109,9 @@ export default function Register_store() {
                     </div>
                     
 
-                    <div className = "p-register-store__container__form__item">
+                    <div className = "p-register-store__container__form__item m-storeForm__item">
                         <label>営業に関する備考</label>
-                        <div className = "p-register-store__container__form__item__input">
+                        <div className = "p-register-store__container__form__item__input m-storeForm__item__input">
                             <span>【例1】定休日：第3水曜日<br></br>【例2】祝日、お盆、年末年始はお休みです。</span>
                             <textarea name="business_memo" ref={register}/>
                         </div>
