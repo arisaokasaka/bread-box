07b593d361b185eabcbad98f0c76b20e1c6194ed
arisa <arisa.okasaka@gmail.com>
2021-02-15 17:27:20 +0900

    営業日入力部分をリファクタリング（true, falseを取得できるように変更）

--- bread-app/resources/ts/components/molecules/InputSchedule.tsx
+++ bread-app/resources/ts/components/molecules/InputSchedule.tsx
@@ -1,34 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import week from '../../info/Week';
 
-type ScheduleProps = ({
-    Week?: any;
-})
-
-const InputSchedule: React.FC<ScheduleProps> = ({Week}) => {
-    Week = week.week;
-
-    const handleClick = (targetClass) => {
-        let classInfo = document.getElementsByClassName(targetClass);
-        let classInfoArray = Array.from(classInfo);
-        classInfoArray.forEach(el => {
-            if(el.className.includes('active')) {
-                el.classList.remove('active') 
-            } else {
-                el.className += ' active';
-            }
-        });
-    }
+const InputSchedule: React.FC = () => {
+    const Week = week.week;
     
     return(
         <div className = "m-inputSchedule__week">
             {Week.map((day)=>(
                 <div className = "m-inputSchedule__week__day" key={day.id}>
                     <div className = "m-inputSchedule__week__day__heading">
-                        <label htmlFor={day.id}>{day.name}</label>
-                        <div className = {"m-inputSchedule__week__day__heading__btn " + day.class} onClick={()=>handleClick(day.class)}>
-                            <span></span>
-                        </div>
+                        <input type="checkbox" id={day.id+"_checked"}/>
+                        <label htmlFor={day.id+"_checked"}>{day.name}</label>
                     </div>
                     <div className = {"m-inputSchedule__week__day__hours " + day.class} >
                         <input type="time" name={day.id+"_open"} className={day.id+"_open"}/>
