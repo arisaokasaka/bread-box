c82dfc73fa16cf947b940554437ee3cbf98fa05f
arisa <arisa.okasaka@gmail.com>
2021-02-18 23:14:56 +0900

    メニュー画像のファイルサイズ上限を設定・バリデーション追加

--- bread-app/resources/ts/components/molecules/storeEdit/storeEditMenu/MenuCreate.tsx
+++ bread-app/resources/ts/components/molecules/storeEdit/storeEditMenu/MenuCreate.tsx
@@ -11,20 +11,33 @@ const MenuCreate: React.FC = () =>  {
     const { register, handleSubmit, errors} = useForm();
     const { state } = useContext(UserAuthContext);
     const { dispatch } = useContext(StoreInfoContext);
-    const [ file, setFile ] = useState();
+    const [ file, setFile ] = useState(null);
+    const [ fileSize, setFileSize ] = useState(0);
+    let error_fileSize: any = null;
 
     // 送信機能
     const onSubmit = (data) => {
-        data['menu_type'] = 1;
-        data['store_uuid'] = state.uuid;
-        console.log(file);
-        createMenu(data, file);
+        if(fileSize <= 3000000){
+            data['menu_type'] = 1;
+            data['store_uuid'] = state.uuid;
+            console.log(file);
+            createMenu(data, file);
+        }else{
+            alert('画像ファイルが上限サイズ3MBを超えています。圧縮するか、別の画像を選択してください。');
+        }
     }
 
     // 画像ファイル取得
     const onChangeFile = (e) => {
-
         setFile(e.target.files[0]);
+        setFileSize(e.target.files[0].size);
+    }
+
+    // ファイルサイズが3MBを超えていた場合のエラーメッセージ
+    if(fileSize > 3000000){
+        error_fileSize = (
+            <p>ファイルの上限サイズ3MBを超えています。圧縮するか、別の画像を選択してください。</p>
+        );
     }
 
     // store_menusテーブルにレコード作成
@@ -34,8 +47,6 @@ const MenuCreate: React.FC = () =>  {
         for( let el in data){
             dataSubmit.append(el, data[el])
         }
-        console.log('datasubmit')
-        console.log(dataSubmit);
         axios({
             url: '/api/create_store_menu',
             method: "post",
@@ -43,10 +54,10 @@ const MenuCreate: React.FC = () =>  {
         })
         .then(res => {
             getMenuInfo();
-            alert('新しいメニューを追加しました。追加したメニューは、メニュー一覧から確認できます。');
-
+            alert('メニューを追加しました。追加したメニューは、メニュー一覧から確認できます。');
         })
         .catch(errors => {
+            alert('メニューの追加に失敗しました。')
         });
     }
 
@@ -104,6 +115,7 @@ const MenuCreate: React.FC = () =>  {
                         <label htmlFor="bread_img">画像アップロード</label>
                         <div className="m-storeForm__item__input">
                             <input type="file" accept="image/*" onChange={(e)=>onChangeFile(e)}/>
+                            {error_fileSize}
                         </div>
                     </div>
                     <div className="m-storeEdit-menuCreate__container__form__btn m-storeForm__btn">
