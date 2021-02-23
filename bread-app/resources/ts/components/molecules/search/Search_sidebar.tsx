e8a8573300c0b3ab527c87d84bda4143b116dbd5
arisa <arisa.okasaka@gmail.com>
2021-02-24 00:08:09 +0900

    サイドバーのコンポーネント修正

--- bread-app/resources/ts/components/molecules/search/Search_sidebar.tsx
+++ bread-app/resources/ts/components/molecules/search/Search_sidebar.tsx
@@ -1,32 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import districts from '../../../info/Districts';
 import bread_kinds from '../../../info/Bread_kinds';
-import days from '../../../info/Days';
-import hours from '../../../info/Hours';
-import Search_sidebar_item from '../../atoms/Search_sidebar_item';
 
+const Search_sidebar: React.FC = () => {
+    const [ condition, setCondition ] = useState({
+        district: '',
+        bread_kind: ''
+    })
+    
+    const sum_district = () => {
+        let info: string = '&di=';
+        districts.districts.map((el)=>{
+            let checkbox = document.getElementById(el.id) as HTMLInputElement;
+            if(checkbox.checked){
+                info = info + checkbox.value + '|';
+            }
+        })
+        setCondition({
+            ...condition,
+            district: info
+        })
+    }
 
-export default function Search_sidebar() {
+    const sum_bread_kind = () => {
+        let info: string = '&br=';
+        bread_kinds.bread_kinds.map((el)=>{
+            let checkbox = document.getElementById(el.id) as HTMLInputElement;
+            if(checkbox.checked){
+                info = info + checkbox.value + '|';
+            }
+        })
+        setCondition({
+            ...condition,
+            bread_kind: info
+        })
+    }
+
     return (
         <div className="m-search-sidebar">
-            <Search_sidebar_item
-            sectionTitle="エリアから探す"
-            sectionContent={districts.districts}
-            />
-            <Search_sidebar_item
-            sectionTitle="パンの種類から探す"
-            sectionContent={bread_kinds.bread_kinds}
-            />
-            <Search_sidebar_item
-            sectionTitle="営業日から探す"
-            sectionContent={days.days}
-            />
-            <Search_sidebar_item
-            sectionTitle="営業時間から探す"
-            sectionContent={hours.hours}
-            />
-            <Link to='/search' className="m-search-sidebar__btn">絞り込む</Link>
+            <div className="m-search-sidebar__item">
+                <h4>キーワードから探す</h4>
+                <input type="text" placeholder="例:あんパン、店名"/>
+            </div>
+            <div className="m-search-sidebar__item">
+                <h4>エリアから探す</h4>
+                <ul>
+                    {districts.districts.map((el)=>{
+                        return (
+                            <li key={"sidebar"+el.id}>
+                                <input id={el.id} name={el.id} className="m-search-sidebar__item__district" type="checkbox" value={el.name} onClick={sum_district}/>
+                                <label htmlFor={el.id}>{el.name}</label>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <div className="m-search-sidebar__item">
+                <h4>パンの種類から探す</h4>
+                <ul>
+                    {bread_kinds.bread_kinds.map((el)=>{
+                        return (
+                            <li key={"sidebar"+el.id}>
+                                <input id={el.id} className="m-search-sidebar__item__bread" type="checkbox" value={el.name} onClick={sum_bread_kind}/>
+                                <label htmlFor={el.id}>{el.name}</label>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <Link to = {{
+                pathname: '/search/',
+                search: condition.district + condition.bread_kind
+                }}
+                className="m-search-sidebar__btn"
+            >
+                絞り込む
+            </Link>
         </div>
     )
 }
+
+export default Search_sidebar;
