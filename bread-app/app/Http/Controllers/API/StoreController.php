160f32e3d1ce6bb1afe031fb04512c42fd1f0126
arisa <arisa.okasaka@gmail.com>
2021-02-25 14:57:37 +0900

    検索結果取得時に、行ってみたい登録状況も取得

--- bread-app/app/Http/Controllers/API/StoreController.php
+++ bread-app/app/Http/Controllers/API/StoreController.php
@@ -38,8 +38,12 @@ class StoreController extends Controller
         $check_uuid = [];
         $check_user_uuid = $user_uuid !== 'null' && $user_uuid;
         if($check_user_uuid) {
-            $user_uuid && $favorite_info = $user->index_favorite($user_uuid);
-            $favorite_info && $favorite_list = json_decode($favorite_info[0]->favorite);    
+            if($user_uuid) {
+                $favorite_info = $user->index_favorite($user_uuid);
+                $favorite_info && $favorite_list = json_decode($favorite_info[0]->favorite);
+                $interested_info = $user->index_interested($user_uuid);
+                $interested_info && $interested_list = json_decode($interested_info[0]->interested);
+            }
         }
         
         if($keyword){
@@ -110,7 +114,15 @@ class StoreController extends Controller
                             $store['favorite_checked'] = true;
                         }
                     }
-                }    
+                }
+
+                if($interested_list){
+                    foreach($interested_list as $interested){
+                        if($store['user_uuid']===$interested){
+                            $store['interested_checked'] = true;
+                        }
+                    }
+                }
             }
         }
         return $result;
@@ -251,5 +263,16 @@ class StoreController extends Controller
             $fileName = 'thumbnail.jpg';
             Storage::putFileAs($path, $file_thumbnail, $fileName, 'public');
         }
+    }
+
+    /**
+     * ログインユーザーのUUID取得
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function get_uuid (Request $request) {
+        $uuid = $request->input('uuid');
+        return $uuid;
     }
 }
