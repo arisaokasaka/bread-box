0f03cd80745246a1b9adf12d90c2e7e4935dc383
arisa <arisa.okasaka@gmail.com>
2021-02-25 18:09:56 +0900

    ユーザー情報を反映するように実装

--- bread-app/resources/ts/components/molecules/user/UserProf.tsx
+++ bread-app/resources/ts/components/molecules/user/UserProf.tsx
@@ -1,35 +1,84 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
+import axios from 'axios';
 import BtnEditUser from '../../atoms/buttons/BtnEditUser';
 import BtnLogout from '../../atoms/buttons/BtnLogout';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHeart, faFlag, faCommentDots} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faFlag, faCommentDots } from '@fortawesome/free-solid-svg-icons';
+import { UserAuthContext } from '../../../contexts/UserAuthContext';
 
-type UserProps = ({
-        UserInfo?: any;
-});
+const UserProf: React.FC = () => {
+    const { state } = useContext(UserAuthContext);
+    const [ info, setInfo ] = useState({});
+    let userInfo: any;
+    let favorite_count: number = 0;
+    let interested_count: number = 0;
 
-const UserProf: React.FC<UserProps> = ({UserInfo}) => {
+    useEffect(()=>{
+        index_user();
+    },[])
+
+    // ユーザー情報取得
+    const index_user = () => {
+        axios.post("/api/index_user", {uuid: state.uuid})
+        .then(res => {
+            setInfo(res.data[0]);
+        })
+        .catch(err => {
+        });
+    }
+
+    if(info){
+        userInfo = info;
+
+        if(userInfo.favorite){
+            let favorite_list = JSON.parse(userInfo.favorite);
+
+            favorite_list ? 
+            favorite_count = favorite_list.length
+            : favorite_count = 0
+
+        }else{
+            favorite_count = 0
+        }
+
+        if(userInfo.interested){
+            let interested_list = JSON.parse(userInfo.interested);
+            
+            interested_list ? 
+            interested_count = interested_list.length
+            : interested_count = 0
+
+        }else{
+            interested_count = 0
+        }
+    }
+
     return(
         <div className = "m-user-prof">
-            {UserInfo.map((el)=>{
-                return(
-                    <div className = "m-user-prof__container" key = {el.uuid}>
-                        <div className = "m-user-prof__container__btn">
-                            <BtnEditUser/>
+            <div className = "m-user-prof__container" key = {userInfo.uuid}>
+                <div className = "m-user-prof__container__btn">
+                    <BtnEditUser/>
+                </div>
+                <div className = "m-user-prof__container__content">
+                    <img src="/images/croissant.jpg" alt="プロフィール画像"/>
+                    <div className = "m-user-prof__container__content__text">
+                        <h3>{userInfo.name}</h3>
+                        <div className = "m-user-prof__container__content__text__item">
+                            <p><span><FontAwesomeIcon icon={faHeart}/></span>お気に入り</p>
+                            <p>{favorite_count}</p>
                         </div>
-                        <div className = "m-user-prof__container__content">
-                            <img src="/images/croissant.jpg" alt="プロフィール画像"/>
-                            <div className = "m-user-prof__container__content__text">
-                                <h3>{el.name}</h3>
-                                <p><span><FontAwesomeIcon icon={faHeart}/></span>お気に入り</p>
-                                <p><span><FontAwesomeIcon icon={faFlag}/></span>行ってみたい</p>
-                                <p><span><FontAwesomeIcon icon={faCommentDots}/></span>レビュー数</p>
-                                <BtnLogout/>
-                            </div>
+                        <div className = "m-user-prof__container__content__text__item">
+                            <p><span><FontAwesomeIcon icon={faFlag}/></span>行ってみたい</p>
+                            <p>{interested_count}</p>
                         </div>
-                    </div>   
-                );
-            })}
+                        <div className = "m-user-prof__container__content__text__item">
+                            <p><span><FontAwesomeIcon icon={faCommentDots}/></span>レビュー数</p>
+                            <p></p>
+                        </div>
+                        <BtnLogout/>
+                    </div>
+                </div>
+            </div>   
         </div>
     );
 }
