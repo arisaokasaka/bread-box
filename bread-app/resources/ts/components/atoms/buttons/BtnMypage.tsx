f699facc2f2d657629930707bf4b2c9b12b86675
arisa <arisa.okasaka@gmail.com>
2021-02-27 11:15:14 +0900

    店舗ログイン時用に、店舗ページへのボタンと管理画面へのボタン追加

--- bread-app/resources/ts/components/atoms/buttons/BtnMypage.tsx
+++ bread-app/resources/ts/components/atoms/buttons/BtnMypage.tsx
@@ -4,31 +4,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faUserCircle} from "@fortawesome/free-solid-svg-icons";
 import { UserAuthContext } from '../../../contexts/UserAuthContext';
 
-export default function BtnMypage_icon() {
+const BtnMypage_icon: React.FC = () => {
     const { state } = useContext(UserAuthContext);
-    let icon: any;
-    
-    if(state.auth === "user"){
-        icon = (
-        <Link to="/user">
-            <FontAwesomeIcon icon={faUserCircle}/>
-            <span>マイページ</span>
-        </Link>
-        );
-    }else if(state.auth === "store"){
-        icon = (
-        <Link to="/store_edit">
-            <FontAwesomeIcon icon={faUserCircle}/>
-            <span>マイページ</span>
-        </Link>
-        );
-    }else{
-        icon = null;
-    }
 
     return (
         <div className = "a-btn-mypage">
-            {icon}
+            {state.auth==="user" &&
+                <Link to="/user">
+                    <FontAwesomeIcon icon={faUserCircle}/>
+                    <span>マイページ</span>
+                </Link>
+            }
         </div>
     )
 }
+
+export default BtnMypage_icon;
