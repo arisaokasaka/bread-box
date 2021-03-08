51fa39ae7ce3e2b89cb1709fd43dc589456a4e1a
arisa <arisa.okasaka@gmail.com>
2021-03-08 12:13:55 +0900

    戻るボタンのコンポーネント修正（Functionを取得できるように変更）

--- bread-app/resources/ts/components/atoms/buttons/BtnBack.tsx
+++ bread-app/resources/ts/components/atoms/buttons/BtnBack.tsx
@@ -3,11 +3,23 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useHistory } from 'react-router-dom';
 
-const BtnBack : React.FC = () => {
+type Props = ({
+    click_function?: Function
+})
+
+const BtnBack : React.FC<Props> = ({click_function}) => {
     const history = useHistory();
 
+    const handleClick = () => {
+        if(click_function) {
+            click_function();
+        } else {
+            history.goBack()
+        }
+    }
+
     return(
-        <a onClick={()=>history.goBack()} className="a-btn-back">
+        <a onClick={handleClick} className="a-btn-back">
             <FontAwesomeIcon icon={faArrowLeft}/>
             <span>戻る</span>
         </a>
