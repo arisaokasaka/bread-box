5dab0c1e98498c211291ce9ce09f1dae3ce7faba
arisa <arisa.okasaka@gmail.com>
2021-03-08 19:18:59 +0900

    店舗情報管理用のReducer, Context削除

--- bread-app/resources/ts/contexts/StoreInfoContext.ts
+++
@@ -1,6 +0,0 @@
-import React, { createContext } from 'react';
-
-export const StoreInfoContext = createContext({} as {
-  stateInfo: any;
-  dispatch: React.Dispatch<React.SetStateAction<any>>;
-});
