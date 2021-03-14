e8e86f35abc82371b485b1bc7b5ad8c6d29aefcf
arisa <arisa.okasaka@gmail.com>
2021-03-14 13:27:51 +0900

    モバイル検索時のパラメータにidも追加

--- bread-app/resources/ts/components/atoms/buttons/BtnSearch_icon.tsx
+++ bread-app/resources/ts/components/atoms/buttons/BtnSearch_icon.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { UserAuthContext } from '../../../contexts/UserAuthContext';
 
-export default function BtnSearch_icon() {
+const BtnSearch_icon: React.FC = () => {
+    const { state } = useContext(UserAuthContext);
     return (
         <div className = "a-btn-search_icon">
-            <Link to={"/search"}>
+            <Link to={"/search?id=" + state.uuid}>
                 <FontAwesomeIcon icon={faSearch}/>
                 <span>検索</span>
             </Link>
         </div>
     )
 }
+
+export default BtnSearch_icon;
